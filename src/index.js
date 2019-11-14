3211a37ac107649406bd81ae003ae490aa7beef8
Rahul Dubey <rahul@roadzen.io>
2019-11-14 20:43:55 +0530

    restart

--- src/index.js
+++ src/index.js
@@ -1,20 +1,47 @@
 const Discord = require('discord.io');
-const express = require('express')
+const http = require('http');
+// const express = require('express')
 
-const app = express()
+// const app = express();
 
 const { DISCORD: { TOKEN }, APP: { PORT } } = require('./configs/vars');
 const { getReply } = require('./app/router');
 const logger = require('./app/utils/logger');
 
 
-app.get('/', function (req, res) {
-  res.send('Discord bot - Health Check')
-})
- 
-app.listen(PORT, () => {
-  logger.info('Server started on port ' + PORT);
-})
+
+
+
+process
+  .on('SIGTERM', shutdown('SIGTERM'))
+  .on('SIGINT', shutdown('SIGINT'))
+  .on('uncaughtException', shutdown('uncaughtException'));
+
+setInterval(() => {}, 1000);
+http.createServer((req, res) => res.end('hi'))
+  .listen(PORT, () => logger.info('Server started on port ' + PORT));
+
+function shutdown(signal) {
+  return (err) => {
+    console.log(`${signal}...`);
+    if (err) console.error(err.stack || err);
+    setTimeout(() => {
+      console.log('...waited 5s, exiting.');
+      process.exit(err ? 1 : 0);
+    }, 5000).unref();
+  };
+}
+
+
+
+
+// app.get('/', function (req, res) {
+//   res.send('Discord bot - Health Check')
+// })
+
+// app.listen(PORT, () => {
+//   logger.info('Server started on port ' + PORT);
+// })
 
 // Initialize Discord Bot
 const bot = new Discord.Client({
@@ -28,7 +55,7 @@ bot.on('ready', (evt) => {
 
 
 bot.on('message', async (user, userID, channelID, message, evt) => {
-  if(bot.id === userID){
+  if (bot.id === userID) {
     return null;
   }
   logger.info('==========');
